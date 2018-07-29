2764c9caabd89ff197f11fd44f087bfdb0016d36
Bryce <bryce@foundrydigital.co>
2018-07-29 18:04:29 -0500

    first commit

---
+++ src/components/Schedule.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { TransitionGroup, CSSTransition } from "react-transition-group";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  Redirect
+} from "react-router-dom";
+
+const Schedule = () => (
+  <div>
+    <ul>
+      <li>6/5 @ Evergreens</li>
+      <li>6/8 vs Kickers</li>
+      <li>6/14 @ United</li>
+    </ul>
+  </div>
+)
+
+export default Schedule
